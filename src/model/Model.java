6e4652897dfe42310cb65c74cfea9940628b24a9
NiklasRolleberg <niklasr93@gmail.com>
2015-05-06 11:44:23 +0200

    lite mer model

--- src/model/Model.java
+++ src/model/Model.java
@@ -1,5 +1,58 @@
 package model;
 
-public class Model {
+import java.awt.Color;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Observable;
+import java.util.Observer;
 
+public class Model extends Observable implements Observer{
+	
+	ArrayList<Conversation> conversations;
+	String myName;
+	Color myColor;
+	
+	
+	
+	/**Containes all conversations and data
+	 */
+	public Model() {
+		conversations = new ArrayList<Conversation>();
+	}
+	
+	/** create new conversation from a connected socket
+	 * @param s
+	 * connected socket
+	 */
+	public void AddConversation(Socket s) {
+		// TODO Create conversation object and add to list
+	}
+	
+	/** create new conversation from ip and port
+	 * @param ip
+	 * ipAddress
+	 * @param port
+	 * port
+	 * @param name
+	 * name of conversation
+	 */
+	public void AddConversation(String ip, int port, String name) {
+		// TODO Create conversation object and add to list
+	}
+	
+	/** remove conversation
+	 * @param name
+	 * name of conversation, might change
+	 */
+	public void removeConversation(String name) {
+		//TODO close and remove the conversation
+	}
+	
+	
+
+	@Override
+	public void update(Observable arg0, Object arg1) {
+		//TODO something. this method will be called when a conversation recives a message
+	}
+	
 }
